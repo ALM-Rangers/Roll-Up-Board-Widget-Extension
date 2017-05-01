921251b53f4a6d3de39a43955beaf86806dbeb58
Mikael Krief <krief_mikael@hotmail.com>
2017-05-01 23:20:11 +0200

    Clen scripts folder by deleting telemetryclient.js

--- src/vsarVSTS-Roll-up-board/scripts/telemetryClientSettings.ts
+++ src/vsarVSTS-Roll-up-board/scripts/telemetryClientSettings.ts
@@ -1,3 +1,15 @@
+// ---------------------------------------------------------------------
+// <copyright file="TelemetryClientSettings.ts">
+//    This code is licensed under the MIT License.
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+//    ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+//    TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+//    PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// </copyright>
+// <summary>
+// </summary>
+// ---------------------------------------------------------------------
+
 import * as tc from "TelemetryClient";
 
 export const settings: tc.TelemetryClientSettings = {
