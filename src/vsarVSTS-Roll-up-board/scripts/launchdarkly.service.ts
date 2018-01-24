1f9d5e4522fd8bb76383a7fd3124d904e9851b6c
Mikael Krief <krief_mikael@hotmail.com>
2018-01-24 19:53:23 +0100

    fix account

--- src/vsarVSTS-Roll-up-board/scripts/launchdarkly.service.ts
+++ src/vsarVSTS-Roll-up-board/scripts/launchdarkly.service.ts
@@ -66,11 +66,12 @@ export class LaunchDarklyService {
     private static hashUserKey(user, hash: boolean, appToken: string, userid: string): IPromise<string> {
         let deferred = $.Deferred<string>();
         if (hash) {
+            let keys = user.key.split(":");
             $.ajax({
                 url: this.UriHashKey,
                 type: "POST",
                 headers: { "Access-Control-Allow-Origin": "*" },
-                data: { account: "" + user.custom.account + "", token: "" + appToken + "" },
+                data: { account: "" + keys[1] + "", token: "" + appToken + "" },
                 success: c => {
                     deferred.resolve(c);
                 },
@@ -87,7 +88,7 @@ export class LaunchDarklyService {
     public static updateUserFeature(appToken: string, user, enable, feature): IPromise<string> {
         let ldproject = this.LdProject;
         let ldenv = this.LdEnv;
-
+        let keys = user.key.split(":");
         let deferred = $.Deferred<string>();
         if (user) {
             $.ajax({
@@ -96,7 +97,7 @@ export class LaunchDarklyService {
                 type: "POST",
                 dataType: "json",
                 headers: { "Access-Control-Allow-Origin": "*" },
-                data: { token: "" + appToken + "", active: "" + enable + "", feature: "" + feature + "", ldproject: "" + ldproject + "", ldenv: "" + ldenv + "", account: "" + user.custom.account + "" },
+                data: { token: "" + appToken + "", active: "" + enable + "", feature: "" + feature + "", ldproject: "" + ldproject + "", ldenv: "" + ldenv + "", account: "" + keys[1] + "" },
                 success: c => {
                     deferred.resolve(c);
                 },
